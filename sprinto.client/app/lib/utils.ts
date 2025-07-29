86ef00cd2fde38a2f7bed94318c72b9a366a1945
Sreejan <sreejan.naru@capsitech.com>
2025-07-29 13:47:11 +0530

    feat(frontend): added custom util function to handle api errors

--- sprinto.client/app/lib/utils.ts
+++ sprinto.client/app/lib/utils.ts
@@ -1,3 +1,5 @@
+import { AxiosError } from "axios";
+import type { NotificationApi } from "~/hooks/useAntNotification";
 
 /**
  * This function returns the initials of a user's full name
@@ -13,4 +15,22 @@ export const getInitials = (fullName: string): string => {
   const last = parts.length > 1 ? parts[parts.length - 1][0] : "";
 
   return (first + last).toUpperCase();
+};
+
+/**
+ * This function handles API errors and displays appropriate messages
+ * @param {Error} error The error object
+ * @param {NotificationApi} _api The notification API
+ * @param {string} message The message to display
+ */
+export const handleApiError = (
+  error: Error,
+  _api: NotificationApi,
+  message: string
+) => {
+  if (error instanceof AxiosError) {
+    _api({ message: error.message, type: "error" });
+  } else {
+    _api({ message: message, type: "error" });
+  }
 };
