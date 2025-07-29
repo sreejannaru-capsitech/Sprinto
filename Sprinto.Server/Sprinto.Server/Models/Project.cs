f45dc39566a9c042d7e56721eb4307d4c2864a5b
Sreejan <sreejan.naru@capsitech.com>
2025-07-29 13:00:22 +0530

    feat(backend): added project auto alias generation

--- Sprinto.Server/Sprinto.Server/Models/Project.cs
+++ Sprinto.Server/Sprinto.Server/Models/Project.cs
@@ -16,6 +16,9 @@ namespace Sprinto.Server.Models
         [BsonElement("title")]
         public string Title { get; set; } = null!;
 
+        [BsonElement("alias")]
+        public string Alias { get; set; } = null!;
+
         [BsonIgnoreIfDefault]
         [BsonElement("description")]
         public string Description { get; set; } = null!;
@@ -40,6 +43,7 @@ namespace Sprinto.Server.Models
         public Project(ProjectDTO dto, string id, string name)
         {
             Title = dto.Title;
+            Alias = GetInitialsFromName(dto.Title);
             Description = dto.Description;
             IsCompleted = dto.IsCompleted ?? false;
             TeamLead = dto.TeamLead;
@@ -52,5 +56,37 @@ namespace Sprinto.Server.Models
         }
 
         public Project() { }
+
+        // Generates alias for a project from it's title
+        private static string GetInitialsFromName(string name)
+        {
+            if (string.IsNullOrWhiteSpace(name))
+                return string.Empty;
+
+            var words = name
+                .Trim()
+                .Split([' '], StringSplitOptions.RemoveEmptyEntries);
+
+            if (words.Length == 1)
+            {
+                var word = words[0];
+                return word.Length >= 3
+                    ? $"{char.ToUpper(word[0])}{char.ToUpper(word[1])}{char.ToUpper(word[^1])}"
+                    : word.ToUpper(); // fallback for short words
+            }
+            else if (words.Length == 2)
+            {
+                var first = words[0];
+                var second = words[1];
+                return $"{char.ToUpper(first[0])}{char.ToUpper(second[0])}{char.ToUpper(second[^1])}";
+            }
+            else
+            {
+                var first = words[0];
+                var second = words[1];
+                var last = words[^1];
+                return $"{char.ToUpper(first[0])}{char.ToUpper(second[0])}{char.ToUpper(last[^1])}";
+            }
+        }
     }
 }
