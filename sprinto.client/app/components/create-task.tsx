7b23cdc43da771e2592f95eb2004a9d1bc0460ca
Sreejan <sreejan.naru@capsitech.com>
2025-07-29 17:14:47 +0530

    feat(frontend): added extra props for different types of button

---
+++ sprinto.client/app/components/create-task.tsx
@@ -0,0 +1,41 @@
+import { useState, type FC, type ReactNode } from "react";
+import TaskForm from "./forms/task-form";
+import { Button, Flex } from "antd";
+import { PencilIcon, PlusIcon } from "~/lib/icons";
+
+interface CreateTaskProps {
+  block?: boolean;
+}
+
+/**
+ * This component renders create-task section
+ * @returns {ReactNode} The CreateTask component
+ */
+const CreateTask: FC<CreateTaskProps> = ({
+  block = false,
+}: CreateTaskProps): ReactNode => {
+  const [taskModalOpen, setTaskModalOpen] = useState<boolean>(false);
+
+  return (
+    <>
+      <TaskForm open={taskModalOpen} onClose={() => setTaskModalOpen(false)} />
+      {block ? (
+        <Button type="default" onClick={() => setTaskModalOpen(true)}>
+          <Flex align="center" gap={4}>
+            <PlusIcon size={20} />
+            <span>Create New</span>
+          </Flex>
+        </Button>
+      ) : (
+        <Button
+          className="header-button"
+          onClick={() => setTaskModalOpen(true)}
+          type="text"
+          icon={<PencilIcon />}
+        />
+      )}
+    </>
+  );
+};
+
+export default CreateTask;
