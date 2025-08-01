0282d153a9a9ce1adc6368e4a2b81a704a3e4320
Sreejan <sreejan.naru@capsitech.com>
2025-08-01 16:14:50 +0530

    feat(frontend): added logout functionality in no project component

--- sprinto.client/app/pages/no-project.tsx
+++ sprinto.client/app/pages/no-project.tsx
@@ -1,13 +1,42 @@
 import { Button, Flex } from "antd";
-import type { ReactNode } from "react";
+import { isAxiosError } from "axios";
+import { useState, type ReactNode } from "react";
+import { useAntNotification } from "~/hooks";
 import CenteredLayout from "~/layouts/centered-layout";
 import { AlertIcon } from "~/lib/icons";
+import { logOut } from "~/lib/server";
 
 /**
  * This component renders no-project section
  * @returns {ReactNode} The NoProject component
  */
 const NoProject = (): ReactNode => {
+  const { _api, contextHolder } = useAntNotification();
+  const [loading, setLoading] = useState<boolean>(false);
+
+  const handleLogout = async () => {
+    setLoading(true);
+    try {
+      const res = await logOut();
+      if (!res.status) {
+        _api({ message: "Failed to log out", type: "error" });
+      } else {
+        window.location.href = "/";
+      }
+    } catch (error) {
+      if (isAxiosError(error)) {
+        _api({
+          message: error.cause?.message ?? error.message,
+          type: "error",
+        });
+      } else {
+        _api({ message: "Failed to log out", type: "error" });
+      }
+    } finally {
+      setLoading(false);
+    }
+  };
+
   return (
     <CenteredLayout>
       <div style={{ textAlign: "center" }}>
@@ -32,8 +61,12 @@ const NoProject = (): ReactNode => {
           Contact your team lead or admin
         </p>
         <Flex gap={4} justify="center">
-          <Button type="primary">Reload</Button>
-          <Button>Log out</Button>
+          <Button type="primary" onClick={() => window.location.reload()}>
+            Reload
+          </Button>
+          <Button onClick={handleLogout} loading={loading}>
+            Log out
+          </Button>
         </Flex>
       </div>
     </CenteredLayout>
